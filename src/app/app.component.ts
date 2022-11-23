1a682c77f8bbadc5e6afd1daefadabd10fe58168
Keny Pau Vas <keny_pauccarima@usmp.pe>
2022-11-23 16:59:33 -0500

    ...

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  
-  valor1: string ='10';
-  valor2: string = '20';
-  resultadoP: number;
+export class AppComponent implements OnInit{
 
+  screen = "";
+  a:any;
+  b:any;
+  c:any;
+  d="";
+  e:"";
+
+
+  ngOnInit(): void {
+    
+  }
   
+  //Calculadora
+
+  enterValue(value:string){
+    
+    if ((this.b=="+")||(this.b=="-")||(this.b=="*")||(this.b=="/")) {
+      this.d= this.d+ value;
+      this.screen = this.screen +value;
+      this.c= this.d;
+
+      console.log(this.b, this.c)
+    }else{
+
+      this.screen = this.screen + value;
+      this.a = this.screen;
+
+      console.log(this.a)
+    }
+
+  }
+
 }
